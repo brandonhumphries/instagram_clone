aaff1f4bd73dabaca90836dd1434c3f4ffbff335
Brandon Humphries <brandonhumphries@Brandons-MacBook-Air.local>
2018-07-25 15:59:27 -0400

    added button functionality to move through images in lightbox

--- main.js
+++ main.js
@@ -10,115 +10,70 @@ var images = [
 
 
 var container = document.querySelector('.main-photos');
+var currentI;
 
 for (var i = 0; i < images.length; i++) {
-    var image = images[i];
-    var newImage = document.createElement('img');
-    newImage.setAttribute('src', image.url);
-    newImage.classList.add('photo');
+    (function () {
+        var image = images[i];
+        var currentNum = i;
+        var newImage = document.createElement('img');
+        newImage.setAttribute('src', image.url);
+        newImage.classList.add('photo');
 
-    var caption = document.createElement('p');
-    caption.textContent = image.caption;
-    caption.classList.add('caption');
+        var caption = document.createElement('p');
+        caption.textContent = image.caption;
+        caption.classList.add('caption');
 
-    var listItem = document.createElement('li');
-    listItem.appendChild(newImage);
-    listItem.appendChild(caption);
-    listItem.classList.add('photo-container');
-    listItem.setAttribute('data-index', i);
-    
-    container.appendChild(listItem);
+        var listItem = document.createElement('li');
+        listItem.appendChild(newImage);
+        listItem.appendChild(caption);
+        listItem.classList.add('photo-container');
+
+        var lightBoxImage = document.querySelector(".lightbox-photo");
+        var lightBoxCaption = document.querySelector(".lightbox-caption");
+        var lightBox = document.querySelector(".lightbox-main");
+
+        var clickHandler = function () {
+            currentI = currentNum;
+            lightBoxImage.setAttribute('src', image.url);
+            lightBoxCaption.textContent = image.caption;
+            lightBox.classList.remove("hidden");
+        }
+
+        listItem.addEventListener('click', clickHandler);
+
+        
+        container.appendChild(listItem);
+
+
+    })();
 }
 
+var leftButton = document.querySelector('.left-button');
+var rightButton = document.querySelector('.right-button');
+var lightBoxImage = document.querySelector(".lightbox-photo");
+var lightBoxCaption = document.querySelector(".lightbox-caption");
+var imageBack = function () {
+    currentI = (currentI + images.length - 1) % images.length;
+    lightBoxImage.setAttribute('src', images[currentI].url);
+    lightBoxCaption.textContent = images[currentI].caption;
+};
 
-// var lightBox = function() {
+var imageForward = function () {
+    currentI = (currentI + images.length + 1) % images.length;
+    lightBoxImage.setAttribute('src', images[currentI].url);
+    lightBoxCaption.textContent = images[currentI].caption;
+};
 
-// };
-// var imageTest = document.querySelector('.photo-container');
-
-// imageTest.addEventListener("click", function(event) {
-//     console.log('It works');
-// });
-
-// var imageTest = document.querySelector('.photo-container');
-// var target = document.querySelector('[url]');
-
-// imageTest.addEventListener("click", function(event) {
-//     event.preventDefault();
-//     target.setAttribute('src', imageTest.getAttribute('url'));
-//     console.log('It works');
-// });
-
-
-// var lightBox = function() {
-
-// };
-// var imageTest = document.querySelectorAll('.photo-container');
-// var lightButton = document.querySelector('.light-button');
-
-
-// lightButton.addEventListener('click', function (event) {
-//     var lightBox = document.querySelector('.lightbox-main');
-//     lightBox.classList.add('hidden');
-// });
-
-// imageTest.forEach(function (element) {
-//     element.addEventListener('click', function (event) {
-//         event.preventDefault();
-//         var selectedImage = event.target.getAttribute('src');
-//         // target.setAttribute('src', (selectedImage));
-//         var lightBoxImage = document.querySelector('.lightbox-photo');
-//         lightBoxImage.setAttribute('src', selectedImage);
-//         var lightBoxMain = document.querySelector('.lightbox-main');
-//         lightBoxMain.classList.remove('hidden');
-//         // console.log('It works');
-//         // console.log(target);
-//         // console.log('a')
-//         // console.log(imageTest);
-//         // console.log(imageTest.getAttribute('src'));
-//     // target.classList.toggle('.photo-container')
-//     });
-// });
-
-
-// var lightBox = document.querySelector('.lightbox')
+leftButton.addEventListener('click', imageBack);
+rightButton.addEventListener('click', imageForward);
 
 
 
-var clickTest = document.querySelectorAll(".photo-container");
 var lightButton = document.querySelector(".light-button");
 
-lightButton.addEventListener('click', function(event){
+lightButton.addEventListener('click', function(){
     var lightBox = document.querySelector(".lightbox-main");
     lightBox.classList.add("hidden");
 });
 
-clickTest.forEach(function (element) {
-    element.addEventListener('click', function(event) {
-        event.preventDefault();
-        
-        console.log(element.textContent);
-        console.log(event.target);
-        console.log('Click Registered');
-        console.log(event.currentTarget.getAttribute('data-index'));
-        
-        var selectedImage = event.target.getAttribute('src');
-        var selectedCaption = element.textContent;
-        var selectedIndex = event.target.getAttribute('data-index');
-        
-        console.log(selectedImage);
-        console.log(selectedCaption);
-        
-        var lightBoxImage = document.querySelector(".lightbox-photo");
-        lightBoxImage.setAttribute('src', selectedImage);
-        
-        var lightBoxCaption = document.querySelector(".lightbox-caption");
-        lightBoxCaption.textContent = selectedCaption;
-        console.log(lightBoxCaption.textContent);
-
-        var lightBox = document.querySelector(".lightbox-main");
-        lightBox.classList.remove("hidden");
-
-    });
-    
-});
